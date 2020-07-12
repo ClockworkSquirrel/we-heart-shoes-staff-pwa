1e498958a53091249f125ef61316b9b3218d4ea8
ClockworkSquirrel <ClockworkSquirrel@users.noreply.github.com>
2020-07-12 18:51:12 +0100

    Update icons+manifest
    * Various icon sizes now available
    * Updated manifest to reflect new icon sizes, added permissons key and specified language.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import * as serviceWorker from "./serviceWorker"
+
 import App from "./components/app"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  
+
   document.querySelector("#app")
 )
+
+serviceWorker.register()
