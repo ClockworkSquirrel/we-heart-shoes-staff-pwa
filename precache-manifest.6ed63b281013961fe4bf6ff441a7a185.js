46ad3eb429927b7d99451348ae7628d9a01f9ee6
ClockworkSquirrel <ClockworkSquirrel@users.noreply.github.com>
2020-07-21 18:24:45 +0100

    Updates

---
+++ precache-manifest.6ed63b281013961fe4bf6ff441a7a185.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e096540977cb0f93541e6612f2f98e",
+    "url": "/we-heart-shoes-staff-pwa/index.html"
+  },
+  {
+    "revision": "4e3cf45eda7291fa39a2",
+    "url": "/we-heart-shoes-staff-pwa/static/js/2.e0c3c176.chunk.js"
+  },
+  {
+    "revision": "92eea8049c1a87906322",
+    "url": "/we-heart-shoes-staff-pwa/static/js/main.651a9f92.chunk.js"
+  },
+  {
+    "revision": "9abdf9da71adf9197344",
+    "url": "/we-heart-shoes-staff-pwa/static/js/runtime~main.8ba85df4.js"
+  }
+]);
